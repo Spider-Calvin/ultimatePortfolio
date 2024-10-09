b1cdacf85067ffa5348d90382e416f3a3b86505e
spider-calvin <calvindas7@gmail.com>
2024-10-10 01:38:12 +0530

    eslint and prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		rules: {
+			'react/jsx-no-target-blank': 'off',
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'prettier/prettier': [
+				'error',
+				{
+					arrowParens: 'avoid',
+					bracketSameLine: true,
+					bracketSpacing: true,
+					singleQuote: true,
+					trailingComma: 'all',
+					semi: true,
+					tabWidth: 2,
+					useTabs: true,
+					jsxSingleQuote: true,
+					proseWrap: 'preserve',
+					printWidth: 100,
+					htmlWhitespaceSensitivity: 'css',
+				},
+			],
+			'react-hooks/rules-of-hooks': 'off',
+			'react-hooks/exhaustive-deps': 'warn',
+			'tailwindcss/classnames-order': 'warn',
+			'tailwindcss/no-custom-classname': 'off',
+			'react/prop-types': 'off',
+		},
+	},
+];
